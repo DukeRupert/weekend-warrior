9cd37e35f56c2c34a4e9d00bbb0d6dafd91bf398
dukerupert <logan@fireflysoftware.dev>
2024-10-31 18:35:51 +0000

    Renders multiple schedules

--- handlers.go
+++ handlers.go
@@ -5,6 +5,11 @@ import (
 	"strconv"
 	"github.com/gofiber/fiber/v2"
 )
+
+type TemplateData struct {
+    Calendars []Calendar
+}
+
 // HTTP handler would need to be updated to include the pairs:
 func CalendarHandler(c *fiber.Ctx) error {
 	// Handle url query values
@@ -22,9 +27,26 @@ func CalendarHandler(c *fiber.Ctx) error {
     }
 
     // Generate the calendar for a specific pair set
-    anchorDate := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
-    pairs := GenerateWeekdayPairs(time.Monday, time.Wednesday, anchorDate)
-    cal := GenerateCalendar(year, month, pairs, "JD", 0) // JD = initials, 0 = color index
+    // anchorDate := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+    // pairs := GenerateWeekdayPairs(time.Monday, time.Wednesday, anchorDate)
+    // cal := GenerateCalendar(year, month, pairs, "JD", 0) // JD = initials, 0 = color index
 
-    return c.Render("calendar", cal)
+    // Generate multiple calendars
+    var calendars []Calendar
+
+    anchorDate := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+    // Example: Generate calendars for different pairs
+    pairs1 := GenerateWeekdayPairs(time.Monday, time.Wednesday, anchorDate)
+    cal1 := GenerateCalendar(year, month, pairs1, "JD", 0)
+
+    pairs2 := GenerateWeekdayPairs(time.Tuesday, time.Thursday, anchorDate)
+    cal2 := GenerateCalendar(year, month, pairs2, "AB", 1)
+
+    calendars = append(calendars, cal1, cal2)
+
+    data := TemplateData{
+        Calendars: calendars,
+    }
+
+    return c.Render("calendar", data)
 }
